8d8afedcfd134791aac90497ec7c9723dc57fbbd
marko <marko@nikovic.com>
2025-07-22 13:52:08 -0400

    cleared the unnecessary files

---
+++ lua/plugins/neo_tree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", function()
+      local ft = vim.bo.filetype
+      if ft == "neo-tree" then
+        -- Toggle Neo-tree (close it)
+        vim.cmd("Neotree toggle")
+      else
+        -- Focus Neo-tree (open it if not already)
+        vim.cmd("Neotree reveal")
+      end
+    end, { silent = true })
+  end
+}
