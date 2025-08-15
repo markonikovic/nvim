1bd0f1ff96301848f1e5db29b5a143d10d658526
marko <marko@nikovic.com>
2025-08-14 22:29:07 -0400

    cleaned up a little

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,54 +1,66 @@
 return {
   {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
     config = function()
-      require("mason-lspconfig"). setup({
-        ensure_installed = {"lua_ls", "ts_ls", "astro", "emmet_language_server"}
-        })
-    end
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls", "astro", "emmet_language_server" },
+      })
+    end,
   },
   {
-  "neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.ts_ls.setup({})
       lspconfig.astro.setup({})
       lspconfig.emmet_language_server.setup({
-        filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "pug",
+          "typescriptreact",
+        },
         -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
         -- **Note:** only the options listed in the table are supported.
         init_options = {
-        ---@type table<string, string>
-        includeLanguages = {},
-        --- @type string[]
-        excludeLanguages = {},
-        --- @type string[]
-        extensionsPath = {},
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-        preferences = {},
-        --- @type boolean Defaults to `true`
-        showAbbreviationSuggestions = true,
-        --- @type "always" | "never" Defaults to `"always"`
-        showExpandedAbbreviation = "always",
-        --- @type boolean Defaults to `false`
-        showSuggestionsAsSnippets = false,
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-        syntaxProfiles = {},
-        --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-        variables = {},
+          ---@type table<string, string>
+          includeLanguages = {},
+          --- @type string[]
+          excludeLanguages = {},
+          --- @type string[]
+          extensionsPath = {},
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+          preferences = {},
+          --- @type boolean Defaults to `true`
+          showAbbreviationSuggestions = true,
+          --- @type "always" | "never" Defaults to `"always"`
+          showExpandedAbbreviation = "always",
+          --- @type boolean Defaults to `false`
+          showSuggestionsAsSnippets = false,
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+          syntaxProfiles = {},
+          --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+          variables = {},
         },
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
       vim.diagnostic.config({
         virtual_text = true,
@@ -57,6 +69,6 @@ return {
         update_in_insert = false,
         severity_sort = true,
       })
-    end
-  }
+    end,
+  },
 }
